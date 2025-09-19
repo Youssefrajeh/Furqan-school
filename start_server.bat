0645a8c9903063b33bed63dc900d13702870c95b
Youssef Rajeh <152282967+Youssefrajeh@users.noreply.github.com>
2025-09-19 12:24:08 -0400

    Delete start_server.bat

--- start_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Quran Recitation Logger Server...
-echo.
-echo The server will run on http://localhost:3000
-echo Press Ctrl+C to stop the server
-echo.
-node server.js
-pause
