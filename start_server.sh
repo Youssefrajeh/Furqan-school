10bfdda309c1c7171f121640496be982a7a60ab5
Youssef Rajeh <152282967+Youssefrajeh@users.noreply.github.com>
2025-09-19 12:23:59 -0400

    Delete start_server.sh

--- start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting Quran Recitation Logger Server..."
-echo ""
-echo "The server will run on http://localhost:3000"
-echo "Press Ctrl+C to stop the server"
-echo ""
-node server.js
